30def60436d027c731ee552753c42e690439d46c
piyushb-27 <piyushbajajp@gmail.com>
2025-07-20 02:22:15 +0530

    Solve problem 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n)
+    if (n<0) return "OOPS"
+    else if (n===0) return 0
+    n = parseInt(n)
+    let a = 1
+    let b = 1
+    for (let i=1; i<=n-2; i++) {
+        let temp = b
+        b = a+b
+        a = temp
+    }
 
+    return b
 };
 
 // Do not edit below this line
