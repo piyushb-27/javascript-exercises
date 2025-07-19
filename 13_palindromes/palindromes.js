1c8de697de50e2a84578e3b69c087679035eb4b7
piyushb-27 <piyushbajajp@gmail.com>
2025-07-20 02:10:58 +0530

    Solve problem 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const punctuationMarks = [
+    ".", "?", "!", ",", ";", ":", 
+    "'", '"', "“", "”",
+    "(", ")", "[", "]", "{", "}", "<", ">", 
+    "—", "-", "-", 
+    "…", "@", "#", "$", "%", "&", "*", "/", "\\", "|", "^", "_", "~", "`", "=", "+"
+    ];
 
+    str = str.toLowerCase()
+    let arrOfLetters = (str.split('')).filter((element)=> element!=' ' && !(punctuationMarks.includes(element)))
+    let orgStr = arrOfLetters.join('')
+    arrOfLetters.reverse()
+    reversedString = arrOfLetters.join('')
+    if (reversedString===orgStr) return true
+    else return false
 };
 
 // Do not edit below this line
