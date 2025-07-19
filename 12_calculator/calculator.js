a609d6e0ff3cbef54a05e2f82ca23c7b04e2c72e
piyushb-27 <piyushbajajp@gmail.com>
2025-07-20 01:54:46 +0530

    Solve 12th problem

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return (num1+num2)
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1-num2)
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+	return numArr.reduce((accumulator, currentValue)=> accumulator+currentValue ,0)
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  return numArr.reduce((accumulator, currentValue)=> accumulator*currentValue ,1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return Math.pow(base,exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1
+  else {
+    let fact = 1
+    for (let i=1; i<=num; i++) {
+      fact *= i
+    }
+    return fact
+  }
 };
 
 // Do not edit below this line
