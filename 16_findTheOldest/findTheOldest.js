a794c1387d5d069733a55f15cdaaddaa155bde4b
piyushb-27 <piyushbajajp@gmail.com>
2025-07-20 02:50:58 +0530

    Solve problem 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let newArr = []
+    let age
+    for (let person of people) {
+    if (person.yearOfDeath === undefined) age = 2025-person.yearOfBirth
+    else age = person.yearOfDeath - person.yearOfBirth
+    newArr.push({
+        name:person.name,
+        age
+    })
+    }
+    newArr.sort((elementA,elementB)=> elementB.age - elementA.age)
+    return newArr[0]
 
 };
 
